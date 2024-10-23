fcf3f658b9c1fe13aa2a314e401ee0ff494189b7
kode <narendarkode@gmail.com>
2024-10-22 20:50:05 -0400

    first commit

---
+++ src/main/java/School/District/SchoolInformation/SchoolInformationApplication.java
@@ -0,0 +1,13 @@
+package School.District.SchoolInformation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolInformationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolInformationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/School/District/SchoolInformation/SchoolInformationApplicationTests.java
@@ -0,0 +1,13 @@
+package School.District.SchoolInformation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolInformationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
